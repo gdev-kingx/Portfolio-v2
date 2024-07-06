63d48964a5ac7518e915e5a67273cea3e39d78f7
R Sarveshvarn <151063233+gdev-kingx@users.noreply.github.com>
2024-07-06 23:12:23 +0530

    Add files via upload

---
+++ src/app/data.js
@@ -0,0 +1,123 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "Yoom",
+    description: "A Replica of the 'ZOOM' Application using NextJS",
+    date: "2024-06-02",
+    demoLink: "https://yoom-five-pi.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "Apple Catcher",
+    description: "A classic Apple catcher game created using Phaser.js library",
+    date: "2024-06-03",
+    demoLink: "https://apple-catcher-js.vercel.app/",
+  },
+  {
+    id: 3,
+    name: "Asteroids",
+    description: "The 1979 Atari game Asteroids is available for all to enjoy",
+    date: "2024-06-04",
+    demoLink: "https://1979-asteroids.vercel.app/",
+  },
+  {
+    id: 4,
+    name: "Minimalistic 3d portfolio",
+    description: "A 3D Portfolio using Three.js and Next.js",
+    date: "2024-06-06",
+    demoLink: "https://3dportfolio-website.vercel.app/",
+  },
+  {
+    id: 5,
+    name: "2048",
+    description: "The Ultimate Animated 2048 Game in Reactjs",
+    date: "2024-06-08",
+    demoLink: "https://2048-reactjs.vercel.app/",
+  },
+  {
+    id: 6,
+    name: "Spotify Clone",
+    description: "A Spotify Clone with vuejs and tailwindcss. (Nonsense Version)",
+    date: "2024-06-10",
+    demoLink: "https://sr-spotify.vercel.app/",
+  },
+  {
+    id: 7,
+    name: "Music Player",
+    description: "A Simple Music Player created using Html5, CSS3 and Javascript",
+    date: "2024-06-11",
+    demoLink: "https://isomusic-player.vercel.app/",
+  },
+  {
+    id: 8,
+    name: "Mini Music Player",
+    description: "A Miniature version of the music player",
+    date: "2024-06-11",
+    demoLink: "https://isomini-music-player.vercel.app/",
+  },
+  {
+    id: 9,
+    name: "Currency Converter",
+    description: "Currency Converter in reactjs",
+    date: "2024-06-22",
+    demoLink: "https://ccurrency.vercel.app/",
+  },
+  {
+    id: 10,
+    name: "Space Invaders",
+    description: "The Space invaders Atari Game in Html Canvas, CSS and javascript with Advance graphics and Super weapons",
+    date: "2024-06-24",
+    demoLink: "https://space-invaders-js-v2.vercel.app/",
+  },
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://www.github.com/gdev-kingx",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/snsaru1578",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "X",
+    link: "https://www.x.com/SnSarveshR",
+    icon: "twitter",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "/resume.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
